e691b8ccfd5423304773c5496afdafca9324d921
Gabriel Nogueira <gs.gamestudy@gmail.com>
2024-10-19 21:36:05 -0300

    feat: React App

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+// import { useState } from "react";
+// import { Tweet } from "./components/Tweet";
+import "./App.css"
+import { AppRoutes } from "./Routes"
+
+function App() {
+  // const [tweets, setTweets] = useState<string[]>([
+  //   'Tweet 1',
+  //   'Tweet 2',
+  //   'Tweet 3',
+  //   'Tweet 4',
+  // ])
+
+  // function createTweet() {
+  //   setTweets([...tweets, 'Tweet 5'])
+  // }
+
+  return (
+    <AppRoutes />
+    // <>
+    //   <div>
+    //     {tweets.map(tweet => {
+    //       return <Tweet text={tweet}/>
+    //     })}
+    //     <button type="button" onClick={createTweet}>Adicionar Tweet</button>
+    //   </div>
+    // </>
+  )
+}
+
+export default App
